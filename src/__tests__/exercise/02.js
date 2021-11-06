91f4851cc14c7ef2b73b4b33271e5440d2cf4bf0
vikas yadav <thejsdeveloper@gmail.com>
2021-11-06 21:47:40 +0530

    using toHaveTextContent for text content

--- src/__tests__/exercise/02.js
+++ src/__tests__/exercise/02.js
@@ -2,8 +2,8 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-import ReactDOM from 'react-dom'
 import {render, fireEvent} from '@testing-library/react'
+import {toHaveTextContent} from '@testing-library/jest-dom'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
@@ -12,11 +12,11 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const [decrement, increment] = container.querySelectorAll('button')
   const message = container.firstChild.querySelector('div')
 
-  expect(message.textContent).toBe('Current count: 0')
+  expect(message).toHaveTextContent('Current count: 0')
 
   fireEvent.click(increment)
-  expect(message.textContent).toBe('Current count: 1')
+  expect(message).toHaveTextContent('Current count: 1')
 
   fireEvent.click(decrement)
-  expect(message.textContent).toBe('Current count: 0')
+  expect(message).toHaveTextContent('Current count: 0')
 })
