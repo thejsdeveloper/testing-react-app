f676f8bd718e1233c77f838211c46459f2a90966
vikas yadav <thejsdeveloper@gmail.com>
2021-11-15 19:49:46 +0530

    chap 5 extra credit 3

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -42,10 +42,12 @@ test(`logging in displays the user's username`, async () => {
 
 test('omitting the password results in an error', async () => {
   render(<Login />)
-  const {username, password} = buildLoginForm()
+  const {username} = buildLoginForm()
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.click(screen.getByRole('button', {name: /submit/i}))
   await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
 
-  expect(screen.getByRole('alert')).toHaveTextContent('password required')
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"password required"`,
+  )
 })
